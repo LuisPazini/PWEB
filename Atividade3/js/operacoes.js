0a7f5cf6d13e2cf812742d8c0ff21ffd0ab62bdb
LuisPazini <40778970+LuisPazini@users.noreply.github.com>
2020-03-25 16:34:48 -0300

    Add files via upload

---
+++ Atividade3/js/operacoes.js
@@ -0,0 +1,8 @@
+var primeiroNumero = parseFloat(prompt("Digite o primeiro número: "));
+var segundoNumero = parseFloat(prompt("Digite o segundo número: "));
+
+alert("Soma: " + (primeiroNumero + segundoNumero));
+alert("Subtração: " + (primeiroNumero - segundoNumero));
+alert("Multiplicação: " + (primeiroNumero * segundoNumero));
+alert("Divisão: " + (primeiroNumero / segundoNumero));
+alert("Resto: " + (primeiroNumero % segundoNumero));
