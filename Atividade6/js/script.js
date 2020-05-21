94f3da06e17ba90fcfaa2f7bab083a6f1a8b99b1
LuisPazini <40778970+LuisPazini@users.noreply.github.com>
2020-05-20 22:24:21 -0300

    Add files via upload

---
+++ Atividade6/js/script.js
@@ -0,0 +1,12 @@
+var aluno1 = {
+    ra: "00312572",
+    nome: "Luciano Faraday"
+}
+
+var aluno1 = new Object();
+aluno.ra = "00312572";
+aluno.nome = "Luciano Faraday";
+
+var aluno1 = {};
+aluno.ra = "00312572";
+aluno.nome = "Luciano Faraday";
