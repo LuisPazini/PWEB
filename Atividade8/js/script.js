10da290c31137e18ac6e36f320df1776ae30456f
LuisPazini <40778970+LuisPazini@users.noreply.github.com>
2020-05-27 23:14:20 -0300

    Add files via upload

---
+++ Atividade8/js/script.js
@@ -0,0 +1,13 @@
+var janela = document.getElementById("janela");
+
+function quebrarJanela(){
+    janela.src = "img/janela-quebrada.jpg";
+}
+
+function abrirJanela(){
+    janela.src = "img/janela-aberta.jpg";
+}
+
+function fecharJanela(){
+    janela.src = "img/janela-fechada.jpg";
+}
