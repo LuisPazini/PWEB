7701b2e98050e3caf212a537648d3fe2c5d8c9ac
LuisPazini <40778970+LuisPazini@users.noreply.github.com>
2020-06-06 23:56:21 -0300

    Add files via upload

--- Portifolio/script/script.js
+++ Portifolio/script/script.js
@@ -1,2 +1,12 @@
-var portifolio = document.getElementById("portifolio");
+var frentePortifolio = document.getElementsByClassName("portifolio-frente")[0];
+var versoPortifolio = document.getElementsByClassName("portifolio-verso")[0];
 
+function verVersoDireira(){
+    frentePortifolio.style.transform = "rotateY(180deg)";
+    versoPortifolio.style.transform = "rotateY(360deg)";
+}
+
+function verVersoEsquerda(){
+    frentePortifolio.style.transform = "rotateY(0deg)";
+    versoPortifolio.style.transform = "rotateY(180deg)";
+}
